8f7bd822fc87151a8ad1b1be6a9b53f3e6a3ca94
Dynjashik <62485240+Dynjashik@users.noreply.github.com>
2022-12-13 00:01:21 +0100

    day 2

--- main.py
+++ main.py
@@ -1,16 +1,47 @@
-file = open("advent/1/input.txt", "r")
+file = open("advent/2/new2.txt", "r")
 
-max_cal = 0
-max_current = 0
+my_score = 0
+opp_score = 0
 for line in file:
-    if line != "\n":
-        max_current+= int(line)
-    else:
-        if max_current > max_cal:
-            max_cal= max_current
-        max_current = 0
+  if line[0] == "A" and line[2]=="X":
+      my_score += 4
+      opp_score += 4
 
-print(max_cal)
+  if line[0] == "B" and line[2]=="Y":
+      my_score+= 5
+      opp_score+= 5
+  
+  if line[0] == "C" and line[2]=="Z":
+      my_score+= 6
+      opp_score+= 6
+
+  if line[0] == "A" and line[2]=="Y":
+      my_score += 8
+      opp_score +=1
+
+  if line[0] == "A" and line[2]=="Z":
+      my_score += 3
+      opp_score += 7
+
+  if line[0] == "B" and line[2]=="Z":
+      my_score += 9
+      opp_score += 2
+
+  if line[0] == "B" and line[2]=="X":
+      my_score += 1
+      opp_score += 8
+
+  if line[0] == "C" and line[2]=="X":
+      my_score += 7
+      opp_score += 3
+
+  if line[0] == "C" and line[2]=="Y":
+      my_score += 2
+      opp_score += 9
+
+    
+print(my_score)
+
 
 
 
